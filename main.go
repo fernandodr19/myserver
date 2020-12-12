32a9a974a8f663cd71e3636a4b961a1762fc053a
Fernando Da Rós <fernando.ros@stone.com.br>
2020-12-12 19:03:15 -0300

    add test

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func GetText() string {
@@ -14,6 +15,27 @@ func Sum(numbers []int) int {
 		sum += n
 	}
 	return sum
+}
+
+type Form interface {
+	Area() float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -36,3 +36,22 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func TestArea(t *testing.T) {
+	verifyArea := func(t *testing.T, form Form, expected float64) {
+		result := form.Area()
+		if result != expected {
+			t.Errorf("Result %.2f; Expected %.2f", result, expected)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		verifyArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		verifyArea(t, circle, 314.1592653589793)
+	})
+}
